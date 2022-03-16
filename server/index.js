bd9d7b6b0a86c246a9762b62d042f4c5dc8a5fac
Milan Patel <patelmj1@uci.edu>
2022-03-15 22:42:33 -0700

    users can register & their info is stored in DB. passwords are encrypted.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const bcrypt = require('bcrypt');
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
@@ -24,10 +25,33 @@ app.get('/register', (req, res) => {
     res.render('register.ejs');
 });
 
-app.post('/register', (req, res) => {
+//when user submits register form
+app.post('/register', async (req, res) => {
+    try{
+        //extract email & password. encrypt the password before storing in DB
+        const email = req.body.email;
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
+        //insert the new user into our DB (the users table)
+        const newUser = await pool.query(
+            "INSERT INTO users(email, password) VALUES($1, $2)",
+            [email, hashedPassword],
+            (err, result) => {
+                if (err) {
+                    return console.error("error executing query", err.stack);
+                }
+            }
+        );
+
+        //redirect user to login page after successful registration
+        res.redirect('/login');
+        
+    } catch {
+        res.redirect('/register');
+    }
 });
 
+//when user submits login form
 app.post('/login', (req, res) => {
 
 });
