4faddb7517a6df79f83d1f405fe854ddef96ff08
Milan Patel <patelmj1@uci.edu>
2022-04-08 19:07:28 -0700

    added email functionality. users are now emailed when products they are tracking come in stock. done with the help of gmail api

---
+++ scraper/quickstart.py
@@ -0,0 +1,96 @@
+from __future__ import print_function
+import base64
+from email.mime.text import MIMEText
+
+import os.path
+from venv import create
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+
+def send_message(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message).execute())
+    print("message sent successfully")
+    print(message['id'])
+    return message
+  except HttpError as error:
+    print("an error occurred")
+    print(error)
+
+
+def create_message(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string().encode()).decode()}
+
+
+def helper(sender, to, subject, message_text):
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)
+
+        message = create_message('gputracker85@gmail.com', to, subject, message_text)
+
+        send_message(service, "gputracker85@gmail.com", message)
+
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f'An error occurred: {error}')
+
+
+# if __name__ == '__main__':
+#     main()
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,4 @@
 # Import libraries
-import pandas as pd
 import psycopg2
 from config.config import config
 
@@ -8,9 +7,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+#import my email service from quickstart
+from quickstart import helper
 
+
+#options for our scraper to bypass warnings, to render elements correctly, etc.
 options = Options()
-# options.add_argument('--headless')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--window-size=1920,1200')
+options.add_argument('--no-sandbox')
+options.add_argument('--allow-insecure-localhost')
+options.add_argument('--log-level=3')
 options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
@@ -24,28 +32,82 @@ conn = psycopg2.connect(**params)
 print('Python connected to PostgreSQL!')
 
 
+#this function is called when a tracked GPU comes in stock so we can notify all the users tracking it
+#gid, url to product, product name
+def find_users(gid, url, name):
+    cur = conn.cursor()
+
+    #find all users tracking this gpu
+    cur.execute("SELECT uid FROM userstogpus WHERE gid = (%s)", [gid])
+
+    subject = "IN STOCK: %s" % name
+    message = "Link: %s.\n Note: We are no longer tracking this item for you." % url
+
+    #get their email
+    for row in cur:
+        uid = row[0]
+        cur2 = conn.cursor()
+        cur2.execute("SELECT email FROM users WHERE uid = (%s)", [uid])
+        for i in cur2:
+            email = i[0]
+            helper("gputracker85@gmail.com", email, subject, message)
+        
+        #do not track this product for the user anymore
+        #update userstogpus table
+        cur3 = conn.cursor()
+        cur3.execute("DELETE FROM userstogpus WHERE uid = (%s) AND gid = (%s)", [uid, gid])
+
+        #update trackedgpus table. 1st get how many people are tracking the GPU
+        cur4 = conn.cursor()
+        cur4.execute("SELECT count FROM trackedgpus WHERE gid = (%s)", [gid])
+
+        #if only this user was tracking the GPU, we remove the whole row
+        for i in cur4:
+            cur5 = conn.cursor()
+            count = i[0]
+            
+            #if only this user was tracking the GPU, we remove the whole row
+            if count == 1:
+                print("row deleted from tracked")
+                cur5.execute("DELETE FROM trackedgpus WHERE gid = (%s)", [gid])
+            
+            #else, we just decrement count to correctly reflect how many people are now tracking the GPU
+            else:
+                print("row updated in tracked")
+                cur5.execute("UPDATE trackedgpus SET count = count - 1 WHERE gid = (%s)", [gid])
+            
+
+    
+    return
+
 #create a new cursor
 cur = conn.cursor()
 
 # Read the table
 cur.execute("""
-SELECT url, type FROM gpus WHERE gid IN (SELECT gid FROM trackedgpus);
+SELECT gid, url, type, name FROM gpus WHERE gid IN (SELECT gid FROM trackedgpus);
 """)
 
 #for every gpu being tracked
 for row in cur:
+    #extract information from each row in table
+    gid = row[0]
+    url = str(row[1].rstrip())
+    site = str(row[2].rstrip())
+    name = str(row[3].rstrip())
+
     #go to product page
-    url = str(row[0].rstrip())
     driver.get(url)
 
     #use bestbuy scraper if URL is for bestbuy
-    if(row[1].rstrip() == "bestbuy"):
+    if(site == "bestbuy"):
         content = driver.find_element(By.CLASS_NAME, 'fulfillment-fulfillment-summary').text
         
         if content == "Sold Out":
             print("out of stock")
         else:
             print("in stock")
+            find_users(gid, url, name)
     
     #use newegg scraper if URL is for newegg
     else:
@@ -55,7 +117,8 @@ for row in cur:
             print("out of stock")
         else:
             print("in stock")
-
+            find_users(gid, url, name)
+            
 
 # Close the connection
 conn.commit()
