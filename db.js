36c2fee99abc4dfbc95d5df212719cffabff9e65
Milan Patel <patelmj1@uci.edu>
2022-06-19 18:52:21 -0700

    updated gitignore

---
+++ db.js
@@ -0,0 +1,24 @@
+const Pool = require("pg").Pool;
+
+require("dotenv").config();
+
+const devConfig = {
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE
+};
+
+
+//production configuration
+const proConfig = {
+    connectionString: process.env.DATABASE_URL //this comes from our heroku addons
+}
+
+//if we are in production, use proConfig. else, use devConfig
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
+
+module.exports = pool;
